c932fda590e56a95aa859722e69d0ede09bcd9ad
Jared <jared.jubas.malz@mail.utoront.ca>
2020-02-22 17:05:31 -0500

    Added user page and change password page.

---
+++ password/password_change.js
@@ -0,0 +1,31 @@
+'use strict';
+const log = console.log;
+
+const changePass = document.querySelector('#changePassword')
+
+// Add submit event listener
+changePass.addEventListener('submit', changePassword)
+
+function changePassword(e) {
+    e.preventDefault();
+
+    // Get inputs from the text fields
+    const pass = document.querySelector('#newPass').value
+    const rePass = document.querySelector('#newPassRe').value
+
+    if (pass == '') {
+        location.href = "password_change.html"
+        log("You must enter a valid password.")
+    }
+    // Check if passwords match
+    else if (pass == rePass) {
+        location.href = "../user/user_page.html"
+        log("Your password has been changed.")
+    }
+
+    // Passwords do not match
+    else {
+        location.href = "password_change.html"
+        log("Passwords do not match.")
+    }
+}
