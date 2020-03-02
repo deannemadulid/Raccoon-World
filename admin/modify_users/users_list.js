cbefd9d68f6d707e86744db2bdeb1a33ed5f5fd5
Deanne <deanne.madulid@mail.utoronto.ca>
2020-03-01 21:25:02 -0500

    Connect 'edit user' page with 'users_list'

--- admin/modify_users/users_list.js
+++ admin/modify_users/users_list.js
@@ -2,11 +2,26 @@
 const log = console.log;
 
 const cancel = document.querySelector('#add_user_button')
+const users = document.querySelector('#user_table')
 
 cancel.addEventListener('click', addUserPage)
+users.addEventListener('click', editUserPage)
 
 function addUserPage(e) {
     e.preventDefault();
 
     location.href = "add_new_user.html"
+}
+
+function editUserPage(e) {
+    e.preventDefault();
+    log(e.target.classList)
+
+    // Check if the 'edit user' button was clicked
+    if (e.target.classList.contains('modify_user_button')) {
+    	const userRow = e.target.parentElement.parentElement
+		const user = userRow.cells[1].textContent // username of user to modify
+
+    	location.href = "edit_user.html"
+    }
 }
